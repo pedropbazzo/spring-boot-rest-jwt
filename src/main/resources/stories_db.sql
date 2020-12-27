1786eade5803cd1180041e2f907f79578d623224
Pedro Bazzo <developerpedropbazzo@gmail.com>
2020-12-27 00:25:43 -0300

    sql

---
+++ src/main/resources/stories_db.sql
@@ -0,0 +1,4 @@
+spring.datasource.url = jdbc:mysql://localhost:3306/stories_db?createDatabaseIfNotExist=true&useSSL=false
+ spring.datasource.username = root
+ spring.datasource.password = 1234
+ spring.jpa.hibernate.ddl-auto=update
